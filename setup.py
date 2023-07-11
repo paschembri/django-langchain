f207ee2636be0412521d48f0cfafa2fe6242ae75
Pierre Alexandre SCHEMBRI <pa.schembri@gmail.com>
2023-07-11 16:34:43 +0200

    Added minimal packaging cfg

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 # coding: utf-8
-import os
+from pathlib import Path
 from setuptools import setup, find_packages
 
 
-here = os.path.dirname(__file__)
+here = Path(__file__).parent
 packages = find_packages("src")
 main_package = packages[0]
-
-with open(os.path.join(here, "requirements.txt"), "r") as fd:
-    requirements = [spec for spec in fd.readlines()]
+long_description = (here / "README.md").read_text()
+requirements = (here / "requirements.txt").read_text().split("\n")
 
 setup(
     name="django-langchain",
     version="0.0.1",
     license="All rights reserved. 2023",
     description="Django Langchain",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author=" P.A. SCHEMBRI",
     author_email="pa.schembri@advanced-stack.com",
     url="https://github.com/paschembri/django-langchain",
